5c56ef87b0aa4fd8cbfd6c4d26ec73ddfcfed2b9
Mauricio de Amorim <amorim@timedev.com>
2009-04-15 10:31:57 -0300

    Alteracao da view restaurante.

--- lab/restaurante/app/controllers/restaurantes_controller.rb
+++ lab/restaurante/app/controllers/restaurantes_controller.rb
@@ -2,4 +2,13 @@ class RestaurantesController < ApplicationController
 	def index #Action
 		@restaurantes = Restaurante.all
 	end
+	def destroy
+		@resturante = Restaurante.find( params [:id])
+		@restaurante.destroy
+		#isto não funciona
+		#@mensagem = "Removido com sucesso"
+		#e pode ser resolvido com uma variável "flash"
+		flash[:notice] = "Removido com sucesso"
+		redirect_to :action => 'index'
+	end
 end
