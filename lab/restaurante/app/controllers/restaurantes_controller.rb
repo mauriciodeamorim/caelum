3015f498c3cd410c10afc83bb6deaae842ba8a9d
Mauricio de Amorim <amorim@timedev.com>
2009-04-15 11:36:25 -0300

    Criacao da view new.html.erb.

--- lab/restaurante/app/controllers/restaurantes_controller.rb
+++ lab/restaurante/app/controllers/restaurantes_controller.rb
@@ -11,4 +11,26 @@ class RestaurantesController < ApplicationController
 		flash[:notice] = "Removido com sucesso"
 		redirect_to :action => 'index'
 	end
+	def new
+		@restaurante = Restaurante.new
+	end
+	def create
+	=begin
+#	@restaurante = Restaurante.new
+#	@restaurante.nome = params['nome']
+	=end
+	#poderia ser como um hash
+	#params['restaurante'] #que um hash
+	#entao passa direto na instancia da classe
+	@restaurante = Restaurante.new params[:restaurante]
+#	@restaurante.save # retorna true ou false se der problema de validação/gravação	
+	#o save fica melhor fazendo o tratamento
+	if @restaurante.save
+		flash[:notice]= "Salvo com sucesso"
+		redirect_to :action => 'index'
+	else
+		#renderize a view da action new
+		#se der problema de validação o formulario é mostrado com o último preenchimento
+		render :action => 'new'
+	end
 end
